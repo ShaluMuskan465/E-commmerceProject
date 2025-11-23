0b46902b5625c5a8258adc9943f37456a33e34d1
ShaluMuskan465 <muskanshalu465@gmail.com>
2025-11-23 22:24:33 +0530

    Add files via upload

---
+++ html.java
@@ -0,0 +1,13 @@
+
+    <!DOCTYPE html>
+<html>
+<body>
+    <h2>Shopping System Login</h2>
+<form>
+    Username: <input type="text"><br><br>
+    Password: <input type="password"><br><br>
+  <button>Login</button>
+</form>
+</body>
+</html>
+
